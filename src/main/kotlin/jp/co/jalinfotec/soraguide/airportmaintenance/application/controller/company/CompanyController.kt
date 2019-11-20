f4d3f3d2675684f05cb24602719130724d3c1660
hidetoKOJIMA <hideto.s.kojima@jalinfotec.co.jp>
2019-11-20 14:23:20 +0900

    会社情報変更処理を実装

--- src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/application/controller/company/CompanyController.kt
+++ src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/application/controller/company/CompanyController.kt
@@ -3,11 +3,13 @@ package jp.co.jalinfotec.soraguide.airportmaintenance.application.controller.com
 import jp.co.jalinfotec.soraguide.airportmaintenance.application.form.TaxiCompanyForm
 import jp.co.jalinfotec.soraguide.airportmaintenance.domain.`object`.User
 import jp.co.jalinfotec.soraguide.airportmaintenance.domain.service.AirportCompanyService
+import jp.co.jalinfotec.soraguide.airportmaintenance.infrastructure.entity.AirportEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class CompanyController(
@@ -37,8 +39,25 @@ class CompanyController(
         return "company/company-setting"
     }
 
+    /**
+     * 会社情報変更
+     */
+    @PostMapping("/company/setting")
+    fun postCompanySetting(
+            @AuthenticationPrincipal user: User,
+            @ModelAttribute airportInfo: AirportEntity,
+            model: Model
+    ): String {
 
+        val result = airportCompanyService.updateAirportInformation(airportInfo,user)
+        if(result) {
+            model.addAttribute("airportInfo",airportInfo)
+        }else {
+            model.addAttribute("message","更新に失敗しました。")
+        }
 
+        return "company/company-setting"
+    }
 
 
 
--- src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/domain/service/AirportCompanyService.kt
+++ src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/domain/service/AirportCompanyService.kt
@@ -1,9 +1,11 @@
 package jp.co.jalinfotec.soraguide.airportmaintenance.domain.service
 
+import jp.co.jalinfotec.soraguide.airportmaintenance.domain.`object`.User
 import jp.co.jalinfotec.soraguide.airportmaintenance.infrastructure.entity.AirPortTaxiEntity
 import jp.co.jalinfotec.soraguide.airportmaintenance.infrastructure.entity.AirportEntity
 import jp.co.jalinfotec.soraguide.airportmaintenance.infrastructure.repository.AirportCompanyRepository
 import jp.co.jalinfotec.soraguide.airportmaintenance.infrastructure.repository.AirportTaxiRepository
+import jp.co.jalinfotec.soraguide.airportmaintenance.infrastructure.repository.UserRepository
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
@@ -13,10 +15,14 @@ import org.springframework.transaction.annotation.Transactional
 class AirportCompanyService(
 
     private val airportCompanyRepository: AirportCompanyRepository,
-    private val airportTaxiRepository: AirportTaxiRepository
+    private val airportTaxiRepository: AirportTaxiRepository,
+    private val userRepository: UserRepository
 
 ) {
 
+    /**
+     * 空港会社情報の取得
+     */
     @Transactional
     @Retryable(value = [Exception::class], maxAttempts = 3, backoff = Backoff(delay = 1000))
     fun getCompanyInfo(companyId: String): AirportEntity? {
@@ -29,7 +35,9 @@ class AirportCompanyService(
         }
     }
 
-    //タクシー会社一覧の取得
+    /**
+     * タクシー会社一覧の取得
+     */
     @Transactional(readOnly = true)
     @Retryable(value = [Exception::class], maxAttempts = 3, backoff = Backoff(delay = 1000))
     fun getCompanyList(airportId: String): ArrayList<AirPortTaxiEntity> {
@@ -52,5 +60,25 @@ class AirportCompanyService(
         return taxiList
     }
 
+    /**
+     * 空港会社情報の更新
+     */
+    fun updateAirportInformation(airportInfo: AirportEntity,user: User): Boolean {
 
+        val acc = userRepository.findByCompanyIdAndUsername(user.getCompanyId(),user.username) ?: return false
+        val optional = airportCompanyRepository.findById(acc.companyId)
+
+
+        if (!optional.isPresent) {
+            // 会社情報が見つからない場合は終了
+            return false
+        }
+        val company = optional.get()
+
+        company.companyName = airportInfo.companyName
+        company.contact = airportInfo.contact
+        airportCompanyRepository.save(company)
+
+        return true
+    }
 }
