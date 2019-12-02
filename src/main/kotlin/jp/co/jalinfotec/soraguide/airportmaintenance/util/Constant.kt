7d3e84917c7b941348f74052dde43cd2990156fc
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-12-02 17:01:44 +0900

    通知メールの不要記述の削除

--- src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/util/Constant.kt
+++ src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/util/Constant.kt
@@ -6,69 +6,13 @@ package jp.co.jalinfotec.soraguide.airportmaintenance.util
 object Constant {
 
     const val ACCOUNT_ROCK_COUNT = 5
-    // 環境変数から取得する
-    val FROM_ADDRESS = System.getenv("FROM_ADDRESS")
-
-    enum class MAIL_TYPE { RESERVE, CANCEL, RESET, NONE }
+    enum class MAIL_TYPE { RESET }
 
     val mailSubject = mutableMapOf(
-            MAIL_TYPE.RESERVE to "ご予約が確定しました",
-            MAIL_TYPE.CANCEL to "取消完了のお知らせ",
             MAIL_TYPE.RESET to "アカウントリセットのお知らせ"
     )
 
     val mailContent = mutableMapOf(
-            MAIL_TYPE.RESERVE to
-                    "%name% 様\n" +
-                    "\n" +
-                    "タクシーをご予約いただきありがとうございます。\n" +
-                    "\n" +
-                    "■ご予約内容\n" +
-                    "\n" +
-                    "＜予約番号＞\n" +
-                    "%rsvId%\n" +
-                    "\n" +
-                    "＜予約情報＞\n" +
-                    "乗車日付：%rideOnDate%\n" +
-                    "乗車時刻：%rideOnTime%\n" +
-                    "\n" +
-                    "■ご案内\n" +
-                    "ご予約の変更をご希望のお客様は下記URLから変更いただくか、タクシー会社へ直接ご連絡ください。\n" +
-                    "%url%\n" +
-                    "\n" +
-                    "■お問い合わせ\n" +
-                    "タクシー会社：%companyName%\n" +
-                    "お電話：%companyContact%\n" +
-                    "\n" +
-                    "\n" +
-                    "当メールは送信専用です。\n" +
-                    "\n",
-
-            MAIL_TYPE.CANCEL to
-                    "%name% 様\n" +
-                    "\n" +
-                    "タクシーをご予約いただきありがとうございます。\n" +
-                    "以下のご予約のお取消しを承りました。\n" +
-                    "またのご利用をお待ちしております。\n" +
-                    "\n" +
-                    "■お取消済み内容\n" +
-                    "\n" +
-                    "＜予約番号＞\n" +
-                    "%rsvId%\n" +
-                    "\n" +
-                    "＜予約情報＞\n" +
-                    "乗車日付：%rideOnDate%\n" +
-                    "乗車時刻：%rideOnTime%\n" +
-                    "\n" +
-                    "\n" +
-                    "■お問い合わせ\n" +
-                    "タクシー会社：%companyName%\n" +
-                    "お電話：%companyContact%\n" +
-                    "\n" +
-                    "\n" +
-                    "当メールは送信専用です。\n" +
-                    "\n",
-
             MAIL_TYPE.RESET to
                     "%companyName% 管理者様\n" +
                     "\n" +
--- src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/util/Environment.kt
+++ src/main/kotlin/jp/co/jalinfotec/soraguide/airportmaintenance/util/Environment.kt
@@ -4,5 +4,5 @@ package jp.co.jalinfotec.soraguide.airportmaintenance.util
  * 環境変数
  */
 object Environment {
-
+    val FROM_ADDRESS = System.getenv("FROM_ADDRESS")
 }
